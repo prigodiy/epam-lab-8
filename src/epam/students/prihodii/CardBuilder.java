b6d6271055dcee106837a8220cf250b3d6fcc441
Nikolay Prigodiy <prigodiy.cn@gmail.com>
2014-04-15 15:01:39 +0300

    Update RegistrationSystem.java

---
+++ src/epam/students/prihodii/CardBuilder.java
@@ -0,0 +1,14 @@
+package epam.students.prihodii;
+
+public class CardBuilder {
+
+    private long id;
+    private CardOwnerType cardOwnerType;
+    private CardTimeType cardTimeType;
+    private CardTripValueType cardTripValueType;
+    private Integer cardBalance;
+
+    public CardBuilder() {
+
+    }
+}
